6d854ed6b932132bfe9b003ed21ece23fc450b20
s-seongsik <52439201+s-seongsik@users.noreply.github.com>
2022-01-05 17:30:10 +0900

    blog update eslint 2022-01-05

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220104.1443';
+const cacheName = 'chirpy-20220105.1729';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
