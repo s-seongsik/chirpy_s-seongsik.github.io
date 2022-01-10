06f168b742e95ce4bb639d22b0bf6759b4f80863
s-seongsik <52439201+s-seongsik@users.noreply.github.com>
2022-01-10 10:57:13 +0900

    blog update add 2022-01-10

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1053';
+const cacheName = 'chirpy-20220110.1056';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
