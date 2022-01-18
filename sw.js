80c2d9eec273937b8f5b5b9951a6032b6006ec0a
s-seongsik <sungsik9831@gmail.com>
2022-01-18 23:35:53 +0900

    blog update vue-components 2022-01-18

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220117.1301';
+const cacheName = 'chirpy-20220118.2335';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
