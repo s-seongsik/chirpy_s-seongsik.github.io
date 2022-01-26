661856f4dd55cb7cd01948ef818774fe6c0aab3e
s-seongsik <sungsik9831@gmail.com>
2022-01-26 23:06:41 +0900

    git workflow learngit remote -v! 2022-01-26

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220125.1725';
+const cacheName = 'chirpy-20220126.2304';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
