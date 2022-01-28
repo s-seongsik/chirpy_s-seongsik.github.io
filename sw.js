96f682840b6427ebe1366202bf7106a0d399683d
s-seongsik <sungsik9831@gmail.com>
2022-01-28 13:02:35 +0900

    git-branch update(2) 2022-01-28

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220128.1001';
+const cacheName = 'chirpy-20220128.1150';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
