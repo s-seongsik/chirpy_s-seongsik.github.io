69a56da5e21a8407d1a7c2cd885f4e33bb97750c
s-seongsik <sungsik9831@gmail.com>
2022-02-02 17:20:02 +0900

    2022-02-02 nuxt.js 03 finish

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220202.0011';
+const cacheName = 'chirpy-20220202.1719';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
