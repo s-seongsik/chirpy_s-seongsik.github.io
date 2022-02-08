18ea8857e9670fd90e854a0f01c86a9e3fd43a32
s-seongsik <sungsik9831@gmail.com>
2022-02-08 13:36:17 +0900

    spring boot 지우고 소프트웨어 버전관리 규칙 추가 - 2022-02-08

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220207.1805';
+const cacheName = 'chirpy-20220208.1335';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
