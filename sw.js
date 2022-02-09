6e994bc61f0f860bf8556778f2dd59b54a8b3e97
s-seongsik <sungsik9831@gmail.com>
2022-02-09 13:43:43 +0900

    node.js Express api 구축 (1) 업데이트 - 2022-02-09

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220208.1335';
+const cacheName = 'chirpy-20220209.1342';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
