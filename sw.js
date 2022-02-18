2c55106f321bdeffde61820205618110bece963d
s-seongsik <sungsik9831@gmail.com>
2022-02-18 10:27:54 +0900

    라이프사이클 훅 추가 - 2022-02-18

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220216.1509';
+const cacheName = 'chirpy-20220218.0958';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
