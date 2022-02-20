c9264e67b86b68c5325505292a54767aaaddc663
s-seongsik <sungsik9831@gmail.com>
2022-02-20 20:45:38 +0900

    자바스크립트 동작원리 추가 - 2022-02-20

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220219.0004';
+const cacheName = 'chirpy-20220220.2044';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
