224ce5e647e825d8c476da65a88706beed587280
s-seongsik <sungsik9831@gmail.com>
2022-02-22 23:33:50 +0900

    node.js docker 배포 내용 수정 - 2022-02-22

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220222.2329';
+const cacheName = 'chirpy-20220222.2332';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
