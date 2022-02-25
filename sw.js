575db5b1a85e831749e05d67aa47eb538f8ae24e
s-seongsik <sungsik9831@gmail.com>
2022-02-25 14:19:03 +0900

    callback함수 완료 - 2022-02-25

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220223.0921';
+const cacheName = 'chirpy-20220225.1417';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
